e1677fef5d9d4165bb17a3c416917edb299856d4
jflinn18 <jflinn18@LAB-SCI-214-08.whitworth.edu>
2015-12-04 10:44:09 -0800

    Made program more user friendly

--- P2PNetworkSim/main.cpp
+++ P2PNetworkSim/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 #include "Network.h"
 #include "Nodes.h"
 #include "Simulation.h"
@@ -8,6 +9,33 @@
 int main() {
 	srand(time(NULL));
 	Simulation simulation;
-	simulation.simOne(100, 10);
+	
+	string resp;
+	int dataRedundancy, netSize;
+
+	std::cout << "Which Simulation? (One or Two): ";
+	std::cin >> resp;
+	try {
+		std::cout << "Size of network: ";
+		std::cin >> netSize;
+		std::cout << "Data Redundancy Rate: ";
+		std::cin >> dataRedundancy;
+	}
+	catch (exception ex) {
+		std::cout << "Incorrect type" << std::endl;
+		exit(0);
+	}
+
+	if (resp == "One" || resp == "one")
+		simulation.simOne(netSize, dataRedundancy);
+	else if (resp == "Two" || resp == "two")
+		simulation.simTwo(netSize, dataRedundancy);
+	else {
+		std::cout << "Incorrect Simulation.";
+		exit(0);
+	}
+
+
+	//simulation.simOne(100, 10);
 	//simulation.simTwo(5000, 1);
 }
